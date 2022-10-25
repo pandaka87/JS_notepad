a68372bc7e38b1e22f1209ef093afdae1502f3b6
Samsou1 <sam.moreau0@gmail.com>
2022-10-25 12:00:45 +0200

    Co-authored-by: Gregory LAIGNEAU <GregoryLAIGNEAU@users.noreply.github.com> Co-authored-by: Noellie Peuch <pandaka87@users.noreply.github.com>

--- src/components/MarkdownInput/index.jsx
+++ src/components/MarkdownInput/index.jsx
@@ -2,21 +2,29 @@ import { useState } from 'react';
 
 const MarkdownInput = ({handleMarkdownInput}) => {
   const [search, setSearch] = useState('');
+  const [error, setError] = useState(null)
 
   const handleChange = (e) => {
     setSearch(e.target.value);
+    setError(null)
     handleMarkdownInput(e.target.value);
     e.preventDefault();
   }
 
   const handleSubmit = (e) => {
-    console.log(e)
-    localStorage.setItem(localStorage.length, search)
+    if(search === ''){
+      setError('La note est vide')
+    }else{
+      setError(null)
+      localStorage.setItem(`${localStorage.length}`, search);
+    }
     e.preventDefault();
+    // console.log(localStorage.getItem(`${localStorage.length - 1}`))
   }
 
   return <form onSubmit={handleSubmit} >
     <input type='textarea' onChange={handleChange} value={search}></input><br></br>
+    <p>{error}</p>
     <input type="submit"  value='Save'></input>
   </form>
 }
